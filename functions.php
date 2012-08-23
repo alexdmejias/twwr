be32a2e2b736cd948cf56ecd193e0856073def49
Alexis Mejias <amejias101@qc.cuny.edu>
2012-08-23 01:04:43 -0400

    * major refactoring of structure files. added specific header and footer files for the main page * css

--- functions.php
+++ functions.php
@@ -6,7 +6,28 @@ function my_function_admin_bar(){
 }
 add_filter( 'show_admin_bar' , 'my_function_admin_bar');
 
+function list_situations_func(){
+	$pages = get_pages('parent=2&child_of=2&sort_column=menu_order&title_li=');
+	$list='<ul class="situations_list">';
+	// echo '<ul class="situations_list">';
+	foreach ($pages as $page) {
+		$var = get_post_meta($page->ID,'aka');
+		$list.= '<li><a href="';
+		$list.= get_permalink($page->ID).'" alt="">';
+		if($var[0]!=''){
+			$list.= $var[0];
+		} else {
+			$list.= $page->post_title;
+		}
+		$list.= '</a></li>';
+	}
+	$list.= '</ul>';
+	return $list;
+}
 
-
+function register_shortcodes(){
+	add_shortcode('list_situations','list_situations_func');
+}
+add_action('init','register_shortcodes');
 
 ?>
--- temp-Situations.php
+++ temp-Situations.php
@@ -6,15 +6,26 @@
  */
 ?>
 <?php get_header(); ?>
+<body <?php body_class(); ?>>
+	<div class="container">
+			<div class="row">
+				<div class="span40 header"></div>
+				<div class="span40 poleTop">
+					<h3>[What's <span class="g">Right</span>, What's <span class="r">Wrong</span>]</h3>
+				</div>
+			</div>
 			<div class="row">
 				<div id="content" class="span40">
 					<div class="sidebar span8">
 						<?php get_sidebar(); ?>
 					</div>
-					<div class="cont span32">
+					<div class="main span32">
 						<?php if(have_posts()): ?>
 							<?php while(have_posts()):the_post(); ?>
-								<?php the_content(''); ?>
+								<?php
+									the_content('');
+									
+								?>
 							<?php endwhile; ?>
 						<?php endif; ?>
 					</div><!-- end .cont -->
